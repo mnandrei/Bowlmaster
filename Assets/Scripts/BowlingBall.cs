b78178aef3d93e9c303f2ac6dbb219e7fabdddf3
mnandrei <mnandrei90@gmail.com>
2015-09-28 21:01:38 +0300

    Added bowling ball and created a script that adds an initial velocity to the ball.

---
+++ Assets/Scripts/BowlingBall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BowlingBall : MonoBehaviour {
+
+    public float Speed;
+
+    private Rigidbody _rigidBody;
+    private AudioSource _audioSource;
+
+    void Start ()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    public void Launch()
+    {
+        _rigidBody.velocity = new Vector3(0.0f, 0.0f, Speed);
+        _audioSource.Play();
+    }
+
+    void Update ()
+    {
+	
+	}
+}
